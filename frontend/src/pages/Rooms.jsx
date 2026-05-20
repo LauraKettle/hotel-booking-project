c44abdae4a1ebc9897920a5cf33ae30d1fd80d9b
LauraKett <laurakettle94@hotmail.com>
2026-05-20 10:10:34 +0100

    added rooms API routes

--- frontend/src/pages/Rooms.jsx
+++ frontend/src/pages/Rooms.jsx
@@ -1,18 +1,50 @@
 import rooms from "../data/rooms";
 import RoomCard from "../components/RoomCard";
 import Navbar from "../components/Navbar";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import hotelRoom1 from "../assets/hotelRoom1.avif";
+import hotelRoom2 from "../assets/hotelRoom2.avif";
+import hotelRoom3 from "../assets/hotelRoom3.avif";
+import hotelRoom4 from "../assets/hotelRoom4.jpg";
 
 function Rooms(){
+    const [rooms, setrooms] = useState([]);                //stores room data from API
+
+    const roomImages = {                                    //adding the room images
+        1: hotelRoom1, 
+        2: hotelRoom2, 
+        3: hotelRoom3,
+        4: hotelRoom4
+    };
+
+    useEffect(() => {                                       //running the code when the page loads
+        async function fetchRooms() {
+            try {
+                const response = await axios.get(           //ask backend for the room data
+                    "http://localhost:5050/api/rooms"
+                );
+
+                setrooms(response.data);                    //stores the API data into React state
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        fetchRooms();
+    }, []);
+
+
     return(
         <>
-        
+        <Navbar />
 
         <div className="rooms-page">
             <h1 className="rooms-title">Available Rooms</h1>
 
             <div className="rooms-container">
                 {rooms.map((room) => (
-                    <RoomCard key={room.id} room={room} />
+                    <RoomCard key={room.id} room={{...room, image: roomImages[room.id]}} />
                 ) )}
             </div>
         </div>
--- server/server.js
+++ server/server.js
@@ -12,10 +12,69 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
+//Rooms data
+const rooms = [
+        {
+        id: 1, 
+        name: "standard room",
+        location: "Rome, Italy",
+        price: 120
+        },
+
+        {
+            id: 2, 
+            name: "Deluxe Room",
+            location: "Paris, France",
+            price: 200
+        },
+
+        {
+            id: 3,
+            name: "Family Room",
+            location: "Lisbon, Portugal",
+            price: 250
+        },
+
+        {
+            id: 4, 
+            name: "Double Room",
+            location: "coasta Brava, Spain",
+            price: 175
+        }
+    ];
+
+//home route
+
 app.get("/", (req, res) => {
     res.send("Server is running");
 });
 
-app.listen(PORT, ()=> {
+
+//rooms API
+
+ app.get("/api/rooms", (req, res) => {
+    res.json(rooms);
+});
+
+//single room
+
+app.get("/api/rooms/:id", (req, res) => {
+     const roomId = Number(req.params.id);
+
+    const room = rooms.find((room) => room.id === roomId);
+
+     if (!room) {
+        return res.status(404).json({
+            message: "Room not found"
+        });
+    }
+
+     res.json(room);
+
+});
+   
+//start server
+
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
